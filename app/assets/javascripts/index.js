1ed183870cdbfd71e1ead4568b3e19df885e746b
tommylcameron <tommylcameron@gmail.com>
2017-03-24 20:03:35 +0000

    Updated JS filter fuctions, added auto-populate date when adding a beverage

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,138 +1,27 @@
-function bindFilterToColumn(filterInputId, colClassName) {
-    // body goes here
-    // get filter, bind onclick to debug
-    // bind filter onkeyup to columns with class name
-};
+function bindFilterToColumn(column_slug) {
+    var filter_input_id = column_slug + '_filter';
+    var column_class = 't' + column_slug;
+    var filter = document.getElementById(filter_input_id);
+    filter.onkeyup = function(){
+        var trows = document.getElementsByClassName("trow");
+        Array.prototype.forEach.call(trows, function(row) {
+            var column = row.getElementsByClassName(column_class)[0];
 
-/*bindFilterToColumn('name_filter', 'tname');
-bindFilterToColumn('varietal_filter', 'tvarietal');
-bindFilterToColumn('name_filter', 'tname');
-bindFilterToColumn('name_filter', 'tname');*/
+            var searchString = filter.value.toLowerCase();
+            var columnValue = column.textContent.toLowerCase();
 
-nameFilter = document.getElementById("name_filter");
-nameFilter.onclick = function(){
-    console.log("A click happened");
-};
-
-
-nameFilter.onkeyup = function(){
-    var trows = document.getElementsByClassName("trow");
-    for (f = 0; f < trows.length; f++) {        
-        var row = trows[f];
-        var rowname = row.getElementsByClassName("tname")[0];
-        var nameFilterText = nameFilter.value.toLowerCase();
-        var rowString = row.textContent.toLowerCase();
-        var nameString = rowname.textContent.toLowerCase();
-       //console.log(nameFilterText);
-       //console.log(rowString);
-        if (nameString.includes(nameFilterText)) {
             row.style.display = "";
-           //console.log("displaying row ", f);
-        } else {
-            row.style.display = "none";
-          //console.log("hiding the row ", f);
-        }
+            if (!columnValue.includes(searchString)) {
+                row.style.display = "none";
+            }
+        });
     };
 };
 
-
-varietalFilter = document.getElementById("varietal_filter");
-varietalFilter.onclick = function(){
-    console.log("A click happened");
-};
-
-varietalFilter.onkeyup = function(){
-    var trows = document.getElementsByClassName("trow");
-    for (f = 0; f < trows.length; f++) {        
-        var row = trows[f];
-        var varietalrow = row.getElementsByClassName("tvarietal")[0];
-        var varietalFilterText = varietalFilter.value.toLowerCase();
-        var rowString = row.textContent.toLowerCase();
-        var varietalString = varietalrow.textContent.toLowerCase();
-       //console.log(varitalFilterText);
-       //console.log(rowString);
-        if (varietalString.includes(varietalFilterText)) {
-            row.style.display = "";
-           //console.log("displaying row ", f);
-        } else {
-            row.style.display = "none";
-          //console.log("hiding the row ", f);
-        }
-    };
-};
+var columns = ['name', 'varietal', 'year', 'rating', 'region'];
+columns.forEach(function(column) {
+    bindFilterToColumn(column);
+})
 
 
-yearFilter = document.getElementById("year_filter");
-yearFilter.onclick = function(){
-    console.log("A click happened");
-};
 
-yearFilter.onkeyup = function(){
-    var trows = document.getElementsByClassName("trow");
-    for (f = 0; f < trows.length; f++) {        
-        var row = trows[f];
-        var rowyear = row.getElementsByClassName("tvintage_year")[0];
-        var yearFilterText = yearFilter.value.toLowerCase();
-        var rowString = row.textContent.toLowerCase();
-        var yearString = rowyear.textContent.toLowerCase();
-       //console.log(varitalFilterText);
-       //console.log(rowString);
-        if (yearString.includes(yearFilterText)) {
-            row.style.display = "";
-           //console.log("displaying row ", f);
-        } else {
-            row.style.display = "none";
-          //console.log("hiding the row ", f);
-        }
-    };
-};
-
-ratingFilter = document.getElementById("rating_filter");
-ratingFilter.onclick = function(){
-    console.log("A click happened");
-};
-
-ratingFilter.onkeyup = function(){
-    var trows = document.getElementsByClassName("trow");
-    for (f = 0; f < trows.length; f++) {        
-        var row = trows[f];
-        var rowrating = row.getElementsByClassName("trating")[0];
-        var ratingFilterText = ratingFilter.value.toLowerCase();
-        var rowString = row.textContent.toLowerCase();
-        var ratingString = rowrating.textContent.toLowerCase();
-       //console.log(varitalFilterText);
-       //console.log(rowString);
-        if (ratingString.includes(ratingFilterText)) {
-            row.style.display = "";
-           //console.log("displaying row ", f);
-        } else {
-            row.style.display = "none";
-          //console.log("hiding the row ", f);
-        }
-    };
-};
-
-regionFilter = document.getElementById("region_filter");
-regionFilter.onclick = function(){
-    console.log("A click happened");
-};
-
-regionFilter.onkeyup = function(){
-    var trows = document.getElementsByClassName("trow");
-    for (f = 0; f < trows.length; f++) {        
-        var row = trows[f];
-        var rowregion = row.getElementsByClassName("tcountry")[0];
-        var regionFilterText = regionFilter.value.toLowerCase();
-        var rowString = row.textContent.toLowerCase();
-        var regionString = rowregion.textContent.toLowerCase();
-       //console.log(varitalFilterText);
-       //console.log(rowString);
-        if (regionString.includes(regionFilterText)) {
-            row.style.display = "";
-           //console.log("displaying row ", f);
-        } else {
-            row.style.display = "none";
-          //console.log("hiding the row ", f);
-        }
-    };
-};
